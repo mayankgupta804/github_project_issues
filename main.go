8b7df30970f0679c605e9ac6f6f7c1baebdc220d
mayank <mayank.gupta804@gmail.com>
2019-03-08 01:44:59 +0530

    Handle authentication + pagination scenario when fetching issues

--- main.go
+++ main.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/google/go-github/github"
 )
 
 func main() {
-	ctx := context.Background()
-	client := github.NewClient(nil)
+	client, ctx := authenticateClient()
 	owner := "smartystreets"
 	repoName := "goconvey"
-	totalOpenIssues := TotalOpenIssues(ctx, client, owner, repoName)
-	issuesLast24Hours := OpenedInLast24Hours(ctx, client, owner, repoName)
-	issuesLast7Days := OpenedInLast7Days(ctx, client, owner, repoName)
+	totalOpenIssues := repoIssuesCounter(ctx, client, owner, repoName, 0)
+	issuesLast24Hours := repoIssuesCounter(ctx, client, owner, repoName, -1)
+	issuesLast7Days := repoIssuesCounter(ctx, client, owner, repoName, -7)
 	issuesMoreThan7Days := totalOpenIssues - issuesLast7Days
 	issuesMoreThan24HoursLessThan7Days := issuesLast7Days - issuesLast24Hours
 	fmt.Printf("Total number of open issues: %d\n", totalOpenIssues)
@@ -26,46 +26,53 @@ func main() {
 	fmt.Printf("Total number of issues opened more than 24 hours ago but less than 7 days ago: %d\n", issuesMoreThan24HoursLessThan7Days)
 }
 
-// TotalOpenIssues returns total open issues of a repository
-func TotalOpenIssues(ctx context.Context, client *github.Client, owner string, repoName string) int {
-	opts := &github.IssueListByRepoOptions{State: "open"} // Default option for State is 'open'
-	// opts := &github.IssueListByRepoOptions{State: 'open'} would work the same way as the above
-	issues, resp, err := client.Issues.ListByRepo(ctx, owner, repoName, opts) // Returns a list of all the open issues
+func authenticateClient() (*github.Client, context.Context) {
+	username := "random"
+	password := "random"
+	tp := github.BasicAuthTransport{
+		Username: strings.TrimSpace(username),
+		Password: strings.TrimSpace(password),
+	}
+
+	client := github.NewClient(tp.Client())
+	ctx := context.Background()
+	user, _, err := client.Users.Get(ctx, "")
+
 	if err != nil {
-		log.Fatalf("Error Encountered: %v", err)
+		log.Printf("\nerror: %v\n", err)
 	}
-	if resp.StatusCode != http.StatusOK {
-		log.Println("Couldn't get the list of open issues. Please try again after some time.")
-	}
-	return len(issues)
+
+	fmt.Printf("\n%v\n", github.Stringify(user))
+	return client, ctx
 }
 
-// OpenedInLast24Hours returns total issues opened in the last 24 hours
-func OpenedInLast24Hours(ctx context.Context, client *github.Client, owner string, repoName string) int {
-	now := time.Now()
-	after := now.AddDate(0, 0, -1)
-	opts := &github.IssueListByRepoOptions{Since: after}
-	issues, resp, err := client.Issues.ListByRepo(ctx, owner, repoName, opts)
-	if err != nil {
-		log.Fatalf("Error Encountered: %v", err)
+func repoIssuesCounter(ctx context.Context, client *github.Client, owner string, repoName string, days int) int {
+	var options = &github.IssueListByRepoOptions{State: "open"}
+	if days < 0 {
+		now := time.Now()
+		sinceTime := now.AddDate(0, 0, days)
+		options.Since = sinceTime
 	}
-	if resp.StatusCode != http.StatusOK {
-		log.Println("Couldn't get the list of open issues. Please try again after some time.")
-	}
-	return len(issues)
-}
+	var issuesCount, nextPage = 0, 0
+	var issues []*github.Issue
+	var resp *github.Response
+	var err error
+	for {
+		issues, resp, err = client.Issues.ListByRepo(ctx, owner, repoName, options)
+		nextPage = resp.NextPage
+		issuesCount += len(issues)
 
-// OpenedInLast7Days returns total issues opened more than 7 days ago
-func OpenedInLast7Days(ctx context.Context, client *github.Client, owner string, repoName string) int {
-	now := time.Now()
-	after := now.AddDate(0, 0, -7)
-	opts := &github.IssueListByRepoOptions{Since: after}
-	issues, resp, err := client.Issues.ListByRepo(ctx, owner, repoName, opts)
-	if err != nil {
-		log.Fatalf("Error Encountered: %v", err)
+		if nextPage == 0 {
+			return issuesCount
+		}
+
+		options.Page = nextPage
+
+		if err != nil {
+			log.Fatalf("Error Encountered: %v", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			log.Println("Couldn't get the list of open issues. Please try again after some time.")
+		}
 	}
-	if resp.StatusCode != http.StatusOK {
-		log.Println("Couldn't get the list of open issues. Please try again after some time.")
-	}
-	return len(issues)
 }
