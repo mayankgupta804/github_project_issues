ee83a316b6808702820394431d24fefad2d50e40
mayank <mayank.gupta804@gmail.com>
2019-03-12 00:27:36 +0530

    Add support for aurthentication if username and password are provided

--- githubclient/github_client.go
+++ githubclient/github_client.go
@@ -2,12 +2,37 @@ package client
 
 import (
 	"context"
+	"strings"
 
 	"github.com/google/go-github/github"
 )
 
 // GithubClientContext provides a client and background context to use the Github API
-func GithubClientContext() (*github.Client, context.Context) {
+func GithubClientContext(username string, password string) (*github.Client, context.Context) {
+	if username == "" || password == "" {
+		client, ctx := createEmptyBackGroundContext()
+		return client, ctx
+	}
+
+	// Create transport for authentication
+	tp := github.BasicAuthTransport{
+		Username: strings.TrimSpace(username),
+		Password: strings.TrimSpace(password),
+	}
+
+	client := github.NewClient(tp.Client())
+	ctx := context.Background()
+	user, resp, err := client.Users.Get(ctx, "")
+
+	// If authentication fails, use an empty background context
+	if err != nil || user == nil || resp.StatusCode == 401 {
+		client, ctx := createEmptyBackGroundContext()
+		return client, ctx
+	}
+	return client, ctx
+}
+
+func createEmptyBackGroundContext() (*github.Client, context.Context) {
 	client := github.NewClient(nil)
 	ctx := context.Background()
 	return client, ctx
